1bbdf1a105879e03094f844eb6f37114d010a19d
jinchool <qkrwlscnf2@naver.com>
2023-10-05 12:59:07 +0900

    기본 레이아웃 완성

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//view template engine
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+
+app.use(express.static('public'));
+
+// routing : 경로에 해당하는 파일을 지정하는 방법
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/portfolio", (req, res) => {
+    res.render("portfolio");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
